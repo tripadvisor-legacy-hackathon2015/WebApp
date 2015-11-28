eaa6ca5df9ee17c5f59478b26a32359c7b2d3b83
Gabriel T <gabriel.tapuc@gmail.com>
2015-11-28 13:41:09 -0500

    Added Angular support.

---
+++ TripAdvisor/public/modules/app.js
@@ -0,0 +1,31 @@
+//actual declaration of the angular app
+var app = angular.module('angularApp', [], function ($interpolateProvider) {
+	$interpolateProvider.startSymbol('<%');
+    $interpolateProvider.endSymbol('%>');
+});
+
+app.controller('MainController', ['$scope', function($scope) {
+	$scope.restaurants = [
+		{
+			name: 'La Botega',
+			coordinates: {
+				lat: 45,
+				lon: 33
+			}
+		},
+		{
+			name: 'Tim Hortons',
+			coordinates: {
+				lat: 30,
+				lon: 30
+			}
+		},
+		{
+			name: 'avarra Restaurant',
+			coordinates: {
+				lat: 12.52,
+				lon: 72
+			}
+		}
+	]
+}]);
