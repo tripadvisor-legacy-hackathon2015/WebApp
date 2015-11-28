d53c7f7081b7c8141f9c4b14dd8571d8ccc57b8f
Gabriel T <gabriel.tapuc@gmail.com>
2015-11-28 16:56:52 -0500

    Successfully captured the json object into angular.

--- TripAdvisor/public/js/scripts.js
+++ TripAdvisor/public/js/scripts.js
@@ -2,12 +2,33 @@ var latitude;
 var longitude;
 var map;
 
+var angularApp = angular.module('angularApp', [], function ($interpolateProvider) {
+    $interpolateProvider.startSymbol('<%');
+    $interpolateProvider.endSymbol('%>');
+});
+
+angularApp.controller('MainController', ['$scope', function($scope) {
+    angular.element(document).ready(function () {
+        $(document).on('searchResponse', populateResults);
+    });
+
+    function populateResults (event, data) {
+        $scope.restaurants = data.result;
+        $scope.$apply();
+    }
+}]);
+
 $(document).ready(function () {
     $("#search_box").keyup(function (e) {
         if (e.keyCode == 13) {
             search();
         }
     });
+
+    // var injector = angular.injector(['ng', 'angularApp']);
+    // var $controller = injector.get('$controller');
+    // var AngularCtrl = $controller('MainController');
+
     $(document).on("searchResponse", populateMap);
     $(document).on("searchResponse", populateList);
 });
@@ -41,31 +62,31 @@ function search() {
 }
 
 function populateMap(event, data) {
-	console.log(event);
-	console.log(data);
+    console.log(event);
+    console.log(data);
 }
 
 function populateList(event, data) {
-	//data:
-	//size: number of items
-	//result: array of items
+    
 }
 
 function initMap() {
-	var myLatLng = {lat: 45.3875812, lng: -75.6982142};
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: myLatLng,
-		zoom: 8
-	});
-	placeMarker(myLatLng,"You are here!");
+    var myLatLng = {lat: 45.3875812, lng: -75.6982142};
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        zoom: 8
+    });
+    placeMarker(myLatLng,"You are here!");
 }
 
 function placeMarker(geoLocation, label) {
-	var marker = new google.maps.Marker({
-		position: geoLocation,
-		title: label
-	});
-	marker.setMap(map);
+    var marker = new google.maps.Marker({
+        position: geoLocation,
+        title: label
+    });
+    marker.setMap(map);
 }
+
+
 
 
