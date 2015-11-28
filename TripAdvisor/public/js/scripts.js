3b4d7da68066f495616e539f669e5476a5aa3595
bountytheef <ncstech07@gmail.com>
2015-11-28 11:38:16 -0500

    front end querying JSON obj script

--- TripAdvisor/public/js/scripts.js
+++ TripAdvisor/public/js/scripts.js
@@ -16,13 +16,26 @@ function setLocation(position) {
 
 function search() {
     var searchText = document.getElementById("search_box").value;
-    var serverAddress;
+    var serverAddress= 'http://localhost:3000';
 
     alert();
+        $.getJSON(serverAddress, {
+							search: searchText,
+              latitude: latitude,
+              longitude: longitude
+					}, function (data) {
+						console.log(data);
+            data = {name:"Test", pizza:"bacon", results:25}
+						if (data.results && data.results.length > 0) {
+							//var mySenators = '<p>build:</p>';
+              alert(data.pizza);
 
-    /*$.getJSON(serverAddress, { search: searchText, latitude: latitude, longitude: longitude })
-        .done(function (data) {
-            
-        });*/
+							$.each(data.resuts, function(i, rep) {
+								if ('pizza' == rep.chamber.toLowerCase()) {
+                }
+
+
+						});
+
+				});
 }
-
