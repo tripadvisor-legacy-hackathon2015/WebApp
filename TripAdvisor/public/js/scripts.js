48868566840ff062c5e5f1f2bc739ed4a78c982b
oliverfei <ofei006@uottawa.ca>
2015-11-28 12:21:47 -0500

    added testing file for json parser

--- TripAdvisor/public/js/scripts.js
+++ TripAdvisor/public/js/scripts.js
@@ -17,25 +17,18 @@ function setLocation(position) {
 
 function search() {
     var searchText = document.getElementById("search_box").value;
-    var serverAddress= 'http://localhost:3000';
+    var serverAddress= 'sample_search_doc.json';
 
-    alert();
     $.getJSON(serverAddress, {
         search: searchText,
         latitude: latitude,
         longitude: longitude
     }, function (data) {
-        console.log(data);
-        data = { name: "Test", pizza: "bacon", results: 25 }
-        if (data.results && data.results.length > 0) {
-            //var mySenators = '<p>build:</p>';
-            alert(data.pizza);
-
-            $.each(data.resuts, function (i, rep) {
-                if ('pizza' == rep.chamber.toLowerCase()) {
-                }
+            //alert(JSON.stringify(data));
+            $.each(data, function (i, rep) {
+                if (i == 1)
+                    alert(JSON.stringify(rep));
             });
-        }
     });
  }
 function initMap() {
