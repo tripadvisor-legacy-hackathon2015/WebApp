3c2023550bbe0f8d7e3d9436390b4846f6844744
RowanFerrabee <rowan.ferrabee@gmail.com>
2015-11-27 19:58:17 -0500

    logging query information

--- TripAdvisor/routes/index.js
+++ TripAdvisor/routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/search', function(req, res, next) {
+	//Get searchText, get geolocation
+	var searchText = req.query.searchText; //assigned to sushi
+	var latitude = req.query.lat;
+	var longitude = req.query.lon;
+	console.log("Text: "+searchText+" Lat: "+latitude+" Lon: "+longitude);
+});
+
 module.exports = router;
